53a0dfadff4f51c35128b0212b88da3ad131d89a
jongbeomi <jongbeomi@users.noreply.github.com>
2024-01-18 16:56:15 +0900

    ㅎㅇ
    ㅎㅇ

--- VanGohe_remake/js/script.js
+++ VanGohe_remake/js/script.js
@@ -1,5 +1,19 @@
+/* nav */
 jQuery(function($){
   $(".menu_toggle_btn").click(function(){
-    $(".gup").stop().slideToggle('fast');
+      $(".gup").stop().slideToggle('fast');
+      });
   });
-});
+
+  /* 마우스 호버시 영상 재생 */
+  window.onload = function() {
+      var videos = document.querySelectorAll('.video_zip video');
+      videos.forEach(function(video) {
+          video.addEventListener('mouseover', function() {
+              this.play();
+          });
+          video.addEventListener('mouseout', function() {
+              this.pause();
+          });
+      });
+  };
